0162c2e633ffdc150e87d2867d889d2f8b79168a
Pedro Henrique <pedro.dorini@hotmail.com>
2018-03-06 16:30:34 -0300

    concertar margin do input

--- frontend/chatbot-airport/src/components/Chat.jsx
+++ frontend/chatbot-airport/src/components/Chat.jsx
@@ -4,7 +4,7 @@ import '../css/chat.css'
 export default class Chat extends Component {
   render() {
     return (
-      <div className="container center grey darken-4">
+      <div className="container-center center grey darken-4">
         <div className="row">
           <div className="message-bot col s5 left left-align blue lighten-4">Hi</div>
         </div>
@@ -12,8 +12,8 @@ export default class Chat extends Component {
           <div className="message-user col s5 right right-align teal accent-3">Hi</div>
         </div>
         <div className="row">
-          <input className="input-inline" type="text" placeholder="Type a message here"/>
-          <button className="btn waves-effect waves-light" type="submit">
+          <input className="input-inline left" type="text" placeholder="Type a message here"/>
+          <button className="btn waves-effect waves-light left" type="submit">
             <i className="material-icons valign-wrapper">send</i>
           </button>
         </div>
