077c3c022c5f0fa162828f6fe8ac692855b71556
Pedro Henrique <pedro.dorini@hotmail.com>
2018-03-06 16:23:17 -0300

    adicionar materialize css e alguns estilos ao template

--- frontend/chatbot-airport/src/components/App.jsx
+++ frontend/chatbot-airport/src/components/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Chat from './Chat'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hi</h1>
+        <Chat/>
       </div>
     )
   }
---
+++ frontend/chatbot-airport/src/components/Chat.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import '../css/chat.css'
+
+export default class Chat extends Component {
+  render() {
+    return (
+      <div className="container center grey darken-4">
+        <div className="row">
+          <div className="message-bot col s5 left left-align blue lighten-4">Hi</div>
+        </div>
+        <div className="row">
+          <div className="message-user col s5 right right-align teal accent-3">Hi</div>
+        </div>
+        <div className="row">
+          <input className="input-inline" type="text" placeholder="Type a message here"/>
+          <button className="btn waves-effect waves-light" type="submit">
+            <i className="material-icons valign-wrapper">send</i>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
