1d8fc923a1837cbc8b163f28cf4046686842da7f
Pedro Henrique <pedro.dorini@hotmail.com>
2018-03-07 00:44:24 -0300

    corrigir erro no backend

--- backend/src/config/userConfig.js
+++ backend/src/config/userConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
   username: '0a451bed-7dc0-4fa1-be50-0e33fd8b2d82',
   password: 'bvJ6Zcs6Vqdr',
-  workspace_id: '585fa89d-548b-4b17-b566-3876c9a06eef'
+  workspace_id: '3cde63ad-8c26-44f3-885b-cacd848494f1'
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,10 @@ const user = require('./config/userConfig')
 const app = express()
 const port = 8000
 
+let payload = {
+  workspace_id: user.workspace_id,
+}
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(allowCors)
@@ -15,24 +19,19 @@ const conversation = new Conversation({
   password: user.password,
   version_date: '2018-03-06'
 })
+conversation.message({ workspace_id: payload.workspace_id }, (err, res) => {
+  payload.context = res.context
+  console.log(payload)
+})
 
 app.post('/api/message', (req, res) => {
-  let payload = {
-    workspace_id: user.workspace_id,
-    input: { text: req.body.message || {} }
-  }
+  payload.input = { text: req.body.message }
   conversation.message(payload, (err, data) => {
     if (err) {
      console.log(err)
     }
     payload.context = data.context
-    conversation.message(payload, (err, data) => {
-      if (err) {
-        console.log(err)
-      } else {
-        res.send(data.output.text)
-      }
-    })
+    res.send(data.output.text)
   })
 })
 
